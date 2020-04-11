e980c2629bab4ba4a6c51546dce6b5afca64a743
minetoshi <minetoshi.api@gmail.com>
2020-04-11 22:57:15 +0900

    Initial commit

---
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.janken
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.preference.PreferenceManager
+import android.view.View
+import androidx.core.content.edit
+import com.example.test.R
+import com.example.test.ResultActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        gu.setOnClickListener{ onJankenButtonTapped(it) }
+        choki.setOnClickListener { onJankenButtonTapped(it) }
+        pa.setOnClickListener { onJankenButtonTapped(it) }
+
+    }
+    fun onJankenButtonTapped(view: View?){
+        val intent = Intent(this, ResultActivity::class.java)
+        startActivity(intent)
+    }
+}
